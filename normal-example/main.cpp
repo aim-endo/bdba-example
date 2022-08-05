08f01263e07da70fcebaca573e1769791887054f
Yuma Endo <endo@ai-ms.com>
2022-08-05 20:35:58 +0900

    using qt api

--- normal-example/main.cpp
+++ normal-example/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "mycuda.h"
+#include "myqt.h"
 
 int main(int argc, const char** argv)
 {
     std::cout << "bdba example 1" << std::endl;
     std::cout << "  nvidia driver = " << mycuda::driverVersion() << std::endl;
     std::cout << "  cuda runtime  = " << mycuda::runtimeVersion() << std::endl;
+    std::cout << "  qt version    = " << myqt::qtVersion() << std::endl;
 
     return 0;
 }
